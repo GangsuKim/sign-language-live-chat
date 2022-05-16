cfa5af184b4206065bc2f2f8847d3fadbe560529
GangsuKim <gangsu1813@gmail.com>
2022-05-16 21:26:06 +0900

    Working

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import ssl
 # from requests import request
 # import os
 
-# users = []
+# users = []ss
 
 serverActive = False
 
@@ -24,16 +24,16 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route("/")
 def hello_world():
-    # global serverActive
-    # if not serverActive:
-    #     conf.get_default().auth_token = "22bp1baMLz3H6QgE3t23iVzAIOj_rHpMqB78wrxnasPzERdn"
-    #     http_tunnel = ngrok.connect(5000)
-    #     tunnels = ngrok.get_tunnels()
+    global serverActive
+    if not serverActive:
+        conf.get_default().auth_token = "22bp1baMLz3H6QgE3t23iVzAIOj_rHpMqB78wrxnasPzERdn"
+        http_tunnel = ngrok.connect(5000)
+        tunnels = ngrok.get_tunnels()
 
-    #     for kk in tunnels:
-    #         print(kk)
+        for kk in tunnels:
+            print(kk)
 
-    #     serverActive = True
+        serverActive = True
 
     return render_template('index.html')
 
