961907708394fc2e7ca1df963d7027f0afedc521
GangsuKim <gangsu1813@gmail.com>
2022-05-17 19:55:40 +0900

    Working on SCROLL

---
+++ frontEnd/scroll.js
@@ -0,0 +1,13 @@
+// const slideDiv = document.getElementById('mainText');
+
+// // CONTROLLER
+// const controller = new ScrollMagic.Controller();
+
+// const scene = ScrollMagic.Scene({
+//     duration : 9000,
+//     triggerElement: slideDiv,
+//     triggerHook:0
+// })
+//     .addIndicators()
+//     .setPin(slideDiv)
+//     .addTo(controller)
